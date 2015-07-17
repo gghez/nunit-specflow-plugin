efffb97e6f3b57d9c1baf01a724593d04e1e1402
Vladimir-Clausse <vladimir.clausse@thomsonreuters.com>
2015-07-17 18:03:07 +0200

    Initial revision

---
+++ Property.SpecFlowPlugin/PropertyGenerator.cs
@@ -0,0 +1,181 @@
+﻿using BoDi;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow.Generator;
+using TechTalk.SpecFlow.Generator.Configuration;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Utils;
+
+namespace Property.SpecFlowPlugin
+{
+    /// <summary>
+    /// The PropertyGeneratorPlugin class that registers the Property generator.
+    /// </summary>
+    public class PropertyGeneratorPlugin : IGeneratorPlugin
+    {
+        public void RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration)
+        {
+        }
+
+        public void RegisterCustomizations(ObjectContainer container, SpecFlowProjectConfiguration generatorConfiguration)
+        {
+            container.RegisterTypeAs<PropertyGeneratorProvider, IUnitTestGeneratorProvider>();
+        }
+
+        public void RegisterDependencies(ObjectContainer container)
+        {
+        }
+    }
+
+    /// <summary>
+    /// The property generator itself.
+    /// Inject some <code>[Property("key", "value is this")]</code> attribute to the corresponding Gherkin test where
+    /// <code>@key:value_is_this
+    /// </code>
+    /// </summary>
+    public class PropertyGeneratorProvider : IUnitTestGeneratorProvider
+    {
+        private const string TESTFIXTURE_ATTR = "NUnit.Framework.TestFixtureAttribute";
+        private const string TEST_ATTR = "NUnit.Framework.TestAttribute";
+        private const string ROW_ATTR = "NUnit.Framework.TestCaseAttribute";
+        private const string CATEGORY_ATTR = "NUnit.Framework.CategoryAttribute";
+        private const string TESTSETUP_ATTR = "NUnit.Framework.SetUpAttribute";
+        private const string TESTFIXTURESETUP_ATTR = "NUnit.Framework.TestFixtureSetUpAttribute";
+        private const string TESTFIXTURETEARDOWN_ATTR = "NUnit.Framework.TestFixtureTearDownAttribute";
+        private const string TESTTEARDOWN_ATTR = "NUnit.Framework.TearDownAttribute";
+        private const string IGNORE_ATTR = "NUnit.Framework.IgnoreAttribute";
+        private const string DESCRIPTION_ATTR = "NUnit.Framework.DescriptionAttribute";
+        private const string PROPERTY_ATTR = "NUnit.Framework.Property";
+        private const string PROPERTY_IDENTIFIER_IN_CATEGORY = ":";
+
+        protected CodeDomHelper CodeDomHelper { get; set; }
+
+        public bool SupportsRowTests { get { return true; } }
+        public bool SupportsAsyncTests { get { return false; } }
+
+        public PropertyGeneratorProvider(CodeDomHelper codeDomHelper)
+        {
+            CodeDomHelper = codeDomHelper;
+        }
+
+        public void SetTestClass(TestClassGenerationContext generationContext, string featureTitle, string featureDescription)
+        {
+            CodeDomHelper.AddAttribute(generationContext.TestClass, TESTFIXTURE_ATTR);
+            CodeDomHelper.AddAttribute(generationContext.TestClass, DESCRIPTION_ATTR, featureTitle);
+
+        }
+
+        public void SetTestClassCategories(TestClassGenerationContext generationContext, IEnumerable<string> featureCategories)
+        {
+            foreach (string Category in featureCategories)
+            {
+                int IndexOfPropertyIdentifier = Category.IndexOf(PROPERTY_IDENTIFIER_IN_CATEGORY);
+                if (IndexOfPropertyIdentifier > 0)
+                {
+                    string Key = Category.Substring(0, IndexOfPropertyIdentifier);
+                    string Value = Category.Substring(IndexOfPropertyIdentifier + 1).Replace("_", " ");
+                    CodeDomHelper.AddAttribute(generationContext.TestClass, PROPERTY_ATTR, Key, Value);
+                }
+                else
+                {
+                    CodeDomHelper.AddAttribute(generationContext.TestClass, CATEGORY_ATTR, Category);
+                }
+            }
+        }
+
+        public void SetTestClassIgnore(TestClassGenerationContext generationContext)
+        {
+            CodeDomHelper.AddAttribute(generationContext.TestClass, IGNORE_ATTR);
+        }
+
+        public virtual void FinalizeTestClass(TestClassGenerationContext generationContext)
+        {
+            // by default, doing nothing to the final generated code
+        }
+
+
+        public void SetTestClassInitializeMethod(TestClassGenerationContext generationContext)
+        {
+            CodeDomHelper.AddAttribute(generationContext.TestClassInitializeMethod, TESTFIXTURESETUP_ATTR);
+        }
+
+        public void SetTestClassCleanupMethod(TestClassGenerationContext generationContext)
+        {
+            CodeDomHelper.AddAttribute(generationContext.TestClassCleanupMethod, TESTFIXTURETEARDOWN_ATTR);
+        }
+
+
+        public void SetTestInitializeMethod(TestClassGenerationContext generationContext)
+        {
+            CodeDomHelper.AddAttribute(generationContext.TestInitializeMethod, TESTSETUP_ATTR);
+        }
+
+        public void SetTestCleanupMethod(TestClassGenerationContext generationContext)
+        {
+            CodeDomHelper.AddAttribute(generationContext.TestCleanupMethod, TESTTEARDOWN_ATTR);
+        }
+
+
+        public void SetTestMethod(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string scenarioTitle)
+        {
+            CodeDomHelper.AddAttribute(testMethod, TEST_ATTR);
+            CodeDomHelper.AddAttribute(testMethod, DESCRIPTION_ATTR, scenarioTitle);
+        }
+
+        public void SetTestMethodCategories(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, IEnumerable<string> scenarioCategories)
+        {
+            foreach (string Category in scenarioCategories)
+            {
+                int IndexOfPropertyIdentifier = Category.IndexOf(PROPERTY_IDENTIFIER_IN_CATEGORY);
+                if (IndexOfPropertyIdentifier > 0)
+                {
+                    string Key = Category.Substring(0, IndexOfPropertyIdentifier);
+                    string Value = Category.Substring(IndexOfPropertyIdentifier + 1).Replace("_", " ");
+                    CodeDomHelper.AddAttribute(testMethod, PROPERTY_ATTR, Key, Value);
+                }
+                else
+                {
+                    CodeDomHelper.AddAttribute(testMethod, CATEGORY_ATTR, Category);
+                }
+            }
+        }
+
+        public void SetTestMethodIgnore(TestClassGenerationContext generationContext, CodeMemberMethod testMethod)
+        {
+            CodeDomHelper.AddAttribute(testMethod, IGNORE_ATTR);
+        }
+
+
+        public void SetRowTest(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string scenarioTitle)
+        {
+            SetTestMethod(generationContext, testMethod, scenarioTitle);
+        }
+
+        public void SetRow(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, IEnumerable<string> arguments, IEnumerable<string> tags, bool isIgnored)
+        {
+            var args = arguments.Select(
+                arg => new CodeAttributeArgument(new CodePrimitiveExpression(arg))).ToList();
+
+            // addressing ReSharper bug: TestCase attribute with empty string[] param causes inconclusive result - https://github.com/techtalk/SpecFlow/issues/116
+            var exampleTagExpressionList = tags.Select(t => new CodePrimitiveExpression(t)).ToArray();
+            CodeExpression exampleTagsExpression = exampleTagExpressionList.Length == 0 ?
+                (CodeExpression)new CodePrimitiveExpression(null) :
+                new CodeArrayCreateExpression(typeof(string[]), exampleTagExpressionList);
+            args.Add(new CodeAttributeArgument(exampleTagsExpression));
+
+            if (isIgnored)
+                args.Add(new CodeAttributeArgument("Ignored", new CodePrimitiveExpression(true)));
+
+            CodeDomHelper.AddAttribute(testMethod, ROW_ATTR, args.ToArray());
+        }
+
+        public void SetTestMethodAsRow(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string scenarioTitle, string exampleSetName, string variantName, IEnumerable<KeyValuePair<string, string>> arguments)
+        {
+            // doing nothing since we support RowTest
+        }
+    }
+}
